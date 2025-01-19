aa6fff1b7bda9365c7bf463f9cc374ab8e6d5421
Ava Katushkae <ava8katushka@gmail.com>
2025-01-19 09:36:24 +0000

    Add prop types

--- app/components/Code.tsx
+++ app/components/Code.tsx
@@ -2,7 +2,7 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import { agate } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 
-export default function Code(props) {
+export default function Code(props: { children: string | string[]; }) {
   return (
     <SyntaxHighlighter language="python" style={agate}>
       {props.children}
